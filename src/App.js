78b2bfb332befe14511b6ea3a16dcb487bd40df2
ricardoslv <ricardoslv80@gmail.com>
2020-07-02 17:56:43 +0100

    Added key hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useKey from './useKey';
 
 function App() {
+
+  let keyu = useKey('ArrowUp');
+  let keyd = useKey('ArrowDown');
+  let keyl = useKey('ArrowLeft');
+  let keyr = useKey('ArrowRight');
+  const values = [keyu, keyd, keyl, keyr].map((k) => (k ? 'true' : 'false')).toString()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{values}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
