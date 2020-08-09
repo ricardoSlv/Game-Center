b175ce61b4007415d406a74e0020a462322e9284
ricardoslv <ricardoslv80@gmail.com>
2020-08-09 14:10:40 +0100

    Fix square sizes

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ type point = [number,number]
 
 function makeGameBoard(points:point[],[appleX,appleY]:point,snakeDead:boolean){
   console.log(points,snakeDead)
-  let board = Array.from((new Array(20)),a=>((new Array(20)).fill(<span style={{backgroundColor: 'blue'}}>{'🐟'}</span>)))
-  points.forEach(([x,y])=>board[x][y]=<span style={{backgroundColor: 'green', borderRadius:'10px'}} >{'🐢'}</span>)
-  board[appleX][appleY]=<span style={{backgroundColor: 'red',borderRadius:'10px'}} >{'🍚'}</span>
+  let board = Array.from((new Array(20)),a=>((new Array(20)).fill(<span style={{backgroundColor: 'green',display:'block',width:'5%',height:'fit-content'}}>{'🌲'}</span>)))
+  points.forEach(([x,y])=>board[x][y]=<span style={{backgroundColor:'snow' , borderRadius:'10px',display:'block',width:'5%',height:'auto'}} >{'🦴'}</span>)
+  board[appleX][appleY]=<span style={{backgroundColor: 'red',borderRadius:'10px',display:'block',width:'5%',height:'auto'}} >{'🍖'}</span>
   
   const [headX,headY]=points[0]
-  board[headX][headY]=<span style={{backgroundColor: 'green',borderRadius:'10px'}} >{'🐸'}</span>
+  board[headX][headY]=<span style={{backgroundColor: 'brown',borderRadius:'10px',display:'block',width:'5%',height:'auto'}} >{'🐶'}</span>
   if(snakeDead){
-    board[headX][headY]=<span style={{backgroundColor: 'black',borderRadius:'10px'}} >{'💀'}</span>
+    board[headX][headY]=<span style={{backgroundColor: 'black',borderRadius:'10px',display:'block',width:'5%',height:'auto'}} >{'💀'}</span>
   }
   return board
 }
@@ -49,23 +49,21 @@ function App() {
   }, [pause,dead]);
 
   return (
-    <div >
-      <header >
-        <div style={{width:'fit-content',margin:'10px auto',textAlign:'center',padding:'2px',border:'3px solid black'}}>
+        <div style={{margin:'10px auto',textAlign:'center',padding:'2px',border:'3px solid black',fontSize:'2rem',display:'flex',width:'60%'}}>
+          <div style={{backgroundColor:'green',display:"flex",width:'100%',flexWrap:'wrap'}}>
+          {renderBoard}
+          </div>
+          <div>
           <span>{`Current score: ${board.current.snake.length}`}</span>
           <br/>
           <span>{secs.toFixed(2)}</span>
           <br/>
           <button onClick={()=>{board.current=(new game(20,20));setSecs(0);setPrevSecs(0);setDead(false);setPause(false)}} >New game</button>
-          <button onClick={()=>setPause(p=>!p)} >Pause</button>
-          <div style={{backgroundColor:'blue'}}>
-          {renderBoard.map(l=>l.concat(<br></br>))}
-          </div>
-        </div>
-        <p>{up-down}</p>
+          <button onClick={()=>setPause(p=>!p)} >Pause</button>  <p>{up-down}</p>
         <p>{right-left}</p>
-      </header>
-    </div>
+          </div>
+          
+        </div>
   );
 }
 
