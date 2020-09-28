63eeed64c6566631b0118e78b16b6e69f14944d0
ricardoslv <ricardoslv80@gmail.com>
2020-09-28 21:06:29 +0100

    Add query to function

--- functions/leaderboard.js
+++ functions/leaderboard.js
@@ -2,14 +2,17 @@ let mongodb = require('mongodb');
 const { MongoClient } = mongodb;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@gamesdb.jiemy.mongodb.net?retryWrites=true&w=majority`
 
-async function getSnakeLeaderboard(map) {
+async function getLeaderboard({game, map}) {
   const DBclient = new MongoClient(uri, { useUnifiedTopology: true })
   let leaderboard = []
   try {
     await DBclient.connect();
     const database = DBclient.db(process.env.DB_NAME)
     const collection = database.collection('leaderboard')
-    const leaderboardObj = await collection.findOne({ game: 'snake', map: map })
+    let leaderboardObj = {}
+    let query={game: game||/.*/, map: map||/.*/}
+   
+    leaderboardObj = await collection.find({query}).toArray() 
     leaderboard = leaderboardObj.leaderboard
 
   } catch (error) {
@@ -20,10 +23,9 @@ async function getSnakeLeaderboard(map) {
   return leaderboard
 }
 
-exports.handler = async function handler(event, context, callback) {
-  const lb = await getSnakeLeaderboard(1)
+exports.handler = async function handler(event, _ /*context*/, callback) {
+  const lb = await getLeaderboard(event.queryStringParameters)
   console.log(event)
-  console.log(context)
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(lb)
