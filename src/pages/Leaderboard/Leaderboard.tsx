4e3914424d8007f3a50658c38aa62af5aba03f12
ricardoslv <ricardoslv80@gmail.com>
2020-09-28 23:53:55 +0100

    Fix api return string instead os json

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -5,14 +5,31 @@ export default function Leaderboard() {
     let [snakeLeaderboards, setSnakeLeaderBoards]: [{ name: string, score: number }[][], React.Dispatch<React.SetStateAction<any>>] = useState([[]])
     useEffect(() => {
         async function fetchData() {
-            //const snakeboards = await getSnakeLeaderboards()
-            const snakeboards ='hello'
+            const query = {}
+            let snakeboards: any[] = []
+            try {
+                //const response = await fetch('./api/leaderboard?' + new URLSearchParams(query))
+                const response = await fetch('./api/leaderboard',
+                    {
+                        method: 'GET',
+                        headers: { 'Content-Type': 'application/json' },
+                        redirect: 'follow'
+                    })
+                console.log(response)
+                if (response.status !== 200)
+                    console.log('There was a problem. Status Code: ' + response.status);
+                else
+                    snakeboards = await response.json()
+            }
+            catch (err) {
+                console.log('Error:', err);
+            }
             console.log(snakeboards)
             setSnakeLeaderBoards(snakeboards)
         }
         fetchData();
         return () => { }
-    },[])
+    }, [])
     return (
         <div>
             <h1>Leaderboard</h1>
