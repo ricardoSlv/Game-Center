ed1d62550727da9e236f2a8186dec75f3ba76aef
ricardoslv <ricardoslv80@gmail.com>
2020-09-29 02:19:12 +0100

    Parse api leaderboard fetch method to json

--- functions/leaderboard.js
+++ functions/leaderboard.js
@@ -23,8 +23,7 @@ async function getLeaderboard(query) {
 }
 
 exports.handler = async function handler(event, _ /*context*/, callback) {
-  const lb = await getLeaderboard(event.body)
-  console.log('body ',event.body,typeof(event.body))
+  const lb = await getLeaderboard(JSON.parse(event.body))
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(lb)
