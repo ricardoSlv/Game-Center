efd2ef163e8b8f77dec82fde522b185c206367d9
ricardoslv <ricardoslv80@gmail.com>
2020-09-29 10:43:29 +0100

    Add record function

---
+++ functions/addrecord.js
@@ -0,0 +1,16 @@
+const {updateLeaderboard} = require('./database/db')
+
+exports.handler = async function handler(event, _ /*context*/, callback) {
+    let status={}
+    const {game,map,name,score} = JSON.parse(event.body)
+    const updateStatus = await updateLeaderboard({game,map},name,score)
+
+    if(updateStatus==='sucess')
+        status={updated: true}
+    else
+        status={updated: false}    
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(status)
+    });
+}
--- functions/database/db.js
+++ functions/database/db.js
@@ -4,19 +4,45 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@gamesdb.
 
 
 exports.getLeaderboard =  async function getLeaderboard(query) {
-    const DBclient = new MongoClient(uri, { useUnifiedTopology: true })
-    let leaderboard = []
-    try {
-      await DBclient.connect();
-      const database = DBclient.db(process.env.DB_NAME)
-      const collection = database.collection('leaderboard')
-      
-      leaderboard = await collection.find(query).toArray() 
-  
-    } catch (error) {
-      console.log('Couldn\'t load leaderboard', error)
-    } finally {
-      await DBclient.close()
-    }
-    return leaderboard
+  const DBclient = new MongoClient(uri, { useUnifiedTopology: true })
+  let leaderboard = []
+  try {
+    await DBclient.connect();
+    const database = DBclient.db(process.env.DB_NAME)
+    const collection = database.collection('leaderboard')
+    
+    leaderboard = await collection.find(query).toArray() 
+
+  } catch (error) {
+    console.log('Couldn\'t load leaderboard', error)
+  } finally {
+    await DBclient.close()
   }
+  return leaderboard
+}
+
+exports.updateLeaderboard =  async function updateLeaderboard(query,name,score) {
+  const DBclient = new MongoClient(uri, { useUnifiedTopology: true })
+  let status = ''
+  try {
+    await DBclient.connect();
+    const database = DBclient.db(process.env.DB_NAME)
+    const collection = database.collection('leaderboard')
+  
+    const leaderboard = await collection.find(query).toArray()
+
+    let recordList = leaderboard.leaderboard
+    recordList.pop()
+    recordList.push({name,score})
+    recordList.sort((a,b)=>a.score-b.score)
+    await collection.updateOne(query,{$set:{leaderboard:recordList}})
+
+    status='sucess'
+  } catch (error) {
+    status='error'
+    console.log('Couldn\'t load leaderboard', error)
+  } finally {
+    await DBclient.close()
+  }
+  return status
+}
